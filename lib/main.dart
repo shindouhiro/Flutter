9adac07b596510c4eac27427077c19422e6aa17c
hikaru <274173368@qq.com>
2021-02-02 11:45:20 +0800

    创建container设置背景色边框

--- lib/main.dart
+++ lib/main.dart
@@ -19,11 +19,18 @@ class MyApp extends StatelessWidget {
                            fontSize: 40.0
                            )
                        ),
-                   alignment:Alignment.center,
+                   alignment:Alignment.topLeft,
                    width:500.0,
                    height:400.0,
-                   color: Colors.lightBlue,
-                   )
+                   padding: const EdgeInsets.fromLTRB(10.0,30.0,0.0,0.0),
+                   margin: const EdgeInsets.all(40.0),
+                   decoration:new BoxDecoration(
+                       gradient:const LinearGradient(
+                           colors: [Colors.lightBlue,Colors.greenAccent,Colors.purple]
+                           ),
+                       border: Border.all(width: 2.0,color: Colors.red)
+                       )
+                   ),
                )
            )
     );
